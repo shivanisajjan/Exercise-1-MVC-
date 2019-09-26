619e641650cbb754fd9b614d77e7eb098277634b
shivanisajjan <shivanisajjan269@gmail.com>
2019-09-26 20:48:50 +0530

    first Commit

--- src/main/java/com/stackroute/javaclass/UserController.java
+++ src/main/java/com/stackroute/javaclass/UserController.java
@@ -9,16 +9,17 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
     @RequestMapping(value = "display",method = RequestMethod.GET)
-    public ModelAndView user(ModelMap model)
+    public ModelAndView user(ModelMap model, @RequestParam(value = "firstname") String userName )
     {
         //ApplicationContext context=new ClassPathXmlApplicationContext("dispatcher-servlet.xml");
         //User user=context.getBean("user",User.class);
-        User user = new User("Shivani");
+        User user = new User(userName);
         System.out.println(user.getName());
         ModelAndView modelAndView=new ModelAndView("display");
         modelAndView.addObject("user",user);
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,6 +1,8 @@
 <html>
 <body>
 <form method="GET" action="display">
+  Name:
+  <input type="text" name="firstname" placeholder="Enter name">
   <input type="submit" value="Submit">
 </form>
 </body>
